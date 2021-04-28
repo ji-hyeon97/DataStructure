3997d57d63b015b42a3b2c03e17f858a7934f6aa
ji-hyeon97 <79920930+ji-hyeon97@users.noreply.github.com>
2021-04-28 18:35:35 +0900

    Add files via upload

---
+++ 연결리스트문제_c언어로쉽게풀어쓴자료구조/9번.c
@@ -0,0 +1,67 @@
+//단순연결리스트 입력값->출력값
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct ListNode {
+	int data;
+	struct ListNode *link;
+}ListNode;
+ListNode* insert(ListNode* head, ListNode* pre, int value) { //practice
+	ListNode *p = (ListNode*)malloc(sizeof(ListNode));
+	p->data = value;
+	p->link = pre->link;
+	pre->link = p;
+	return head;
+}
+void insert_last(ListNode **head, int value) {
+	ListNode *p = (ListNode*)malloc(sizeof(ListNode));
+	ListNode *pre = *head;
+	p->data = value;
+	p->link = NULL;
+	if (*head == NULL) {
+		*head = p;
+		return;
+	}
+	while (pre->link != NULL) {
+		pre = pre->link;
+	}
+	pre->link = p;
+	return;
+}
+ListNode* delete_first(ListNode *head) { //practice
+	ListNode* removed;
+	if (head == NULL) {
+		return NULL;
+	}
+	removed = head;
+	head = removed->link;
+	free(removed);
+	return head;
+}
+ListNode* delete(ListNode *head, ListNode* pre) { //pratice
+	ListNode *removed;
+	removed = pre->link;
+	pre->link = removed->link;
+	free(removed);
+	return head;
+}
+void answer(ListNode* head) {
+	printf("생성된 연결 리스트 : ");
+	for (ListNode *p = head; p != NULL; p = p->link) {
+		printf("%d->", p->data);
+	}
+	printf("\n");
+}
+int main(void) {
+	ListNode* list = NULL;
+	int n, t;
+	printf("노드의 개수 : ");
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++) {
+		printf("노드 %d 데이터 : ", i + 1);
+		scanf_s("%d", &t);
+		insert_last(&list, t);
+	}
+	answer(list);
+	return 0;
+}
