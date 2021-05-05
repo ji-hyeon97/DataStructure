2ddc8b4c08adc5bf694b78d8cca34ce51c02e4ef
ji-hyeon97 <79920930+ji-hyeon97@users.noreply.github.com>
2021-05-06 00:15:17 +0900

    Update 트리순회개념정리.c

--- 트리문제_c언어로쉽게풀어쓴자료구조/트리순회개념정리.c
+++ 트리문제_c언어로쉽게풀어쓴자료구조/트리순회개념정리.c
@@ -94,3 +94,79 @@ int main() {
 	inorder(root);
 	return 0;
 }
+
+//레벨순회
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+#define max_size 100
+typedef struct treenode {
+	int data;
+	struct treenode *left, *right;
+}treenode;
+typedef struct queuetype {
+	int queue[max_size];
+	int front, rear;
+}queuetype;
+void init(queuetype *q) {
+	q->front = q->rear=0 ;
+	return;
+}
+int empty(queuetype *q) {
+	return (q->rear == q->front);
+}
+int full(queuetype *q) {
+	return ((q->rear + 1) % max_size == q->front);
+}
+void enqueue(queuetype *q, int item) {
+	if (full(q)) {
+		exit(1);
+	}
+	else {
+		q->rear = (q->rear + 1) % max_size;
+		q->queue[q->rear] = item;
+	}
+	return;
+}
+int dequeue(queuetype *q) {
+	if (empty(q)) {
+		exit(1);
+	}
+	else {
+		q->front = (q->front + 1) % max_size;
+		return q->queue[q->front];
+	}
+}
+void level_inorder(treenode *n) {
+	queuetype q;
+	init(&q);
+	if (n == NULL) {
+		return;
+	}
+	else {
+		enqueue(&q, n);
+		while (!empty(&q)) {
+			n = dequeue(&q);
+			printf("%d ", n->data);
+			if (n->left) {
+				enqueue(&q, n->left);
+			}
+			if (n->right) {
+				enqueue(&q, n->right);
+			}
+		}
+	}
+	return;
+}
+int main() {
+	treenode n1 = { 1,NULL,NULL };
+	treenode n2 = { 4,&n1,NULL };
+	treenode n3 = { 16,NULL,NULL };
+	treenode n4 = { 25,NULL,NULL };
+	treenode n5 = { 20,&n3,&n4 };
+	treenode n6 = { 15,&n2,&n5 };
+	treenode *root = &n6;
+	level_inorder(root);
+	return 0;
+}
