3de2a4b14649ecfec565c4741ec597f60861bdb9
ji-hyeon97 <79920930+ji-hyeon97@users.noreply.github.com>
2021-05-08 00:21:00 +0900

    Add files via upload

---
+++ 트리문제_c언어로쉽게풀어쓴자료구조/18번.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+#define max_size 100
+typedef struct treenode {
+	int data;
+	struct treenode *left, *right;
+}treenode;
+treenode *new_node(int item) {
+	treenode *temp = (treenode*)malloc(sizeof(treenode));
+	temp->data = item;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+treenode *insert_node(treenode *node, int key) {
+	if (node == NULL) {
+		return new_node(key);
+	}
+	if (key < node->data) {
+		node->left = insert_node(node->left, key);
+	}
+	else if (key > node->data) {
+		node->right = insert_node(node->right, key);
+	}
+	return node;
+}
+void inorder(treenode *node) {
+	if (node) {
+		inorder(node->left);
+		printf("%d ", node->data); 
+		inorder(node->right);
+	}
+}
+int main(){
+	int key[11] = {11,3,4,1,56,5,6,2,98,32,23 };
+	treenode *root = NULL;
+	for (int i = 0; i < 11; i++) {
+		root = insert_node(root, key[i]);
+	}
+	inorder(root);
+	return 0;
+}
